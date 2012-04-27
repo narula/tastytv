3f2e6f46bc2bfc3491925ddf7c813f26f5b353d1
Neha Narula <narula@gmail.com>
2012-04-27 19:56:39 -0400

    typo

--- frontend/server.py
+++ frontend/server.py
@@ -18,7 +18,7 @@ def friends():
     return render_template('friends.html')
 
 @app.route("/browse")
-def friends():
+def browse():
     return render_template('browse.html')
     
 if __name__ == "__main__":
